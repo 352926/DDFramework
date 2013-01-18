f1031ccb9c804322d7c2f2517499cab99662528f
352926@qq.com <352926@qq.com>
2013-01-18 17:22:57 +0800

    Add NO_MODULE_FILE

--- lib/Lang/zh-cn.php
+++ lib/Lang/zh-cn.php
@@ -8,6 +8,7 @@
 return array(
     'NO_ACTION' => '非法操作',
     'NO_MODULE' => '模块不存在',
+    'NO_MODULE_FILE' => '模块文件不存在',
     'NO_MYSQL_CONFIG' => '尚未配置数据库',
     'NO_DEFINED_CACHE_DIR' => '未配置缓存目录',
     'NO_EXISTS_CACHE_DIR' => '缓存目录不存在',

d4b4eb96328f96d45a47ae99bb46437b0713b67d
352926@qq.com <352926@qq.com>
2013-02-21 17:08:59 +0800

    * check xdebug if installed when run xdebug_start_trace in index.php

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
  * Date: 13-1-13
  * Time: 13:14
  */
-xdebug_start_trace();
+if (function_exists('xdebug_start_trace')) {
+    xdebug_start_trace();
+}
 define("__ROOT__", dirname(__FILE__));
 require(__ROOT__ . "/lib/Dingding.php");
 $app = new App();
 $app->run();
 register_shutdown_function(function () {
-    print_r("<hr>" . str_replace("\n", "<BR>", file_get_contents(xdebug_stop_trace())));
+    if (function_exists('xdebug_stop_trace'))
+        print_r("<hr>" . str_replace("\n", "<BR>", file_get_contents(xdebug_stop_trace())));
 });

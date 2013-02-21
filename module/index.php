7bcbdbcdca91ded8674ddf1ef15cebded4024fd7
352926@qq.com <352926@qq.com>
2013-02-21 12:57:27 +0800

    * modify

--- module/index.php
+++ module/index.php
@@ -8,18 +8,6 @@ class indexClass extends Module
 {
     function indexAction()
     {
-        echo 'Hello World!';
-//        $ary = array(
-//            'aa' => 'bb'
-//        );
-//        $cache = new DDCache();
-//        $rs = $cache->set($this->module . '-' . $this->action . '.ary', $ary);
-//        $rs = $cache->get($this->module . '-' . $this->action . '.ary');
-//        $rs = $cache->del($this->module . '-' . $this->action . '.ary');
-//        echo "<hr><pre>";
-//        $db = new DB();
-//        $rs = $db->Query("Select * from hdd_tb1");
-//        $rs = $this->params;
-//        print_r(get_included_files());
+        echo "这是网站首页";
     }
 }
